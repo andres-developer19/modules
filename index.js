dacccb4185964e8f4c1a01c43d6f4366e025bb9e
andres.developer19 <andres.developer19@gmail.com>
2023-09-06 11:15:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function initSwiper() {
+var swiper = new Swiper(".mySwiper", {
+navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev",
+},
+});
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+initSwiper();
+});
